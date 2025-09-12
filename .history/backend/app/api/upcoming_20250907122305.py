29584f0b5e6e4a834d82e004ef50f57069796266
Regiesluk <k.regina@traxion.global>
2025-09-11 20:42:25 -0600

    adding files to deploy project

--- .history/backend/app/api/upcoming_20250907122305.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from app.services.spacex import next_launches as get_next_launches
-
-router = APIRouter()
-
-@router.get("")
-async def list_next_launches():
-  data = await get_next_launches()
-  return data
